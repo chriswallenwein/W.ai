f36e35720dcebecbdace68e7849ab419e545b4f5
Christian Wallenwein <chris.wallenwein@gmail.com>
2021-03-13 08:19:50 +0100

    Fully connected layer with and w/o the bias trick

---
+++ W/layer.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class Layer():
+    def __init__(self):
+        return
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def backward(self, x):
+        raise NotImplementedError
+
+class FullyConnectedBiasTrick(Layer):
+
+    def __init__(self, in_dim, out_dim):
+        self.w = 2*np.random.rand(out_dim, in_dim+1) -1
+
+    def forward(self, x):
+        ones = np.ones((1, x.shape[1]))
+        x = np.concatenate((ones, x))
+        y_hat = self.w@x # the bias trick
+        return y_hat
+
+    def backward(self, x):
+        return x
+
+class FullyConnectedNormal(Layer):
+
+    def __init__(self, in_dim, out_dim):
+        self.w = 2*np.random.rand(out_dim, in_dim) -1
+        self.b = 2*np.random.rand(out_dim, 1) -1
+
+    def forward(self, x):
+        y_hat = self.w@x + self.b
+        return y_hat
+
+    def backward(self, x):
+        return x
+
+FullyConnected = FullyConnectedBiasTrick
+
+class Softmax(Layer):
+
+    def __init__(self):
+        return
+
+    def forward(self, x):
+        return
+
+    def backward(self, x):
+        return 

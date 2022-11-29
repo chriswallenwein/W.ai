6dd80209e63f8f027cc99070a73604d0e34a3be8
cwallenwein <chris.wallenwein@gmail.com>
2022-11-29 15:50:41 +0100

    visualize model + model weights

--- code/visualization.py
+++ code/visualization.py
@@ -8,56 +8,49 @@ import matplotlib.pyplot as plt
 #   - plt.imshow(predictions, origin="lower", extent=[-10,10,-10,10], cmap=plt.cm.RdYlGn)
 #   - Each pixel is showing one color. The color is determined by the class with the highest probability for that pixel
 #     then calculate max(class probabilities for that pixel) * color of class with highest probability
-
 # TODO: Display current loss of model
+# TODO: Add 3D plot for 3D input data
 
-class Visualization():
-    def __init__(self, x_min=-10, x_max=10, y_min=-10, y_max=10):
-        self.x_min = x_min
-        self.x_max = x_max
-        self. y_min = y_min
-        self.y_max = y_max
-        self.ax = plt.axes()
-    
-    def visualize_model(self, model, data_x, data_y):
+class Visualize():
+
+    @staticmethod
+    def model(model, features, labels):
         if model.layers[0].in_dim != 2:
             raise ValueError("Input dimensions of layer must equal 2")
-        self.visualize_model_weights(model)
-        self.visualize_data(data_x, data_y)
+        
+        boundaries = Visualize.calculate_boundaries(features)
+        ax = plt.axes()
+        Visualize.visualize_model_weights(ax, model, boundaries)
+        Visualize.visualize_data(ax, features, labels)
+        plt.xlabel("x_1")
+        plt.ylabel("x_2")
 
-    def visualize_layer(self, layer, data_x, data_y):
-        # The layer must have 2-dimensional input so it can be visualized in a 2D plot
-        if layer.in_dim != 2:
-            raise ValueError("Input dimensions of layer must equal 2")
-        self.visualize_layer_weights(layer)
-        self.visualize_data(data_x, data_y)
-
-    def visualize_model_weights(self, model):
-        xs = np.linspace(self.x_min, self.x_max, 250)
-        ys = np.linspace(self.y_min, self.y_max, 250)
+    @staticmethod
+    def visualize_model_weights(ax, model, boundaries):
+        x_min, x_max, y_min, y_max = boundaries
+        xs = np.linspace(x_min, x_max, 250)
+        ys = np.linspace(y_min, y_max, 250)
         mesh_input = np.array(np.meshgrid(xs, ys))
         # flatten mesh to allow forward pass
         mesh_input = mesh_input.reshape(2, 250*250)
         predictions = model.forward(mesh_input)
         # undo flatten to for squared image
         predictions = predictions.reshape(250,250)
-        self.ax.contourf(xs, ys, predictions, levels=100, cmap=plt.cm.RdYlGn)
-    
-    def visualize_layer_weights(self, layer):
-        xs = np.linspace(self.x_min, self.x_max, 250)
-        ys = np.linspace(self.y_min, self.y_max, 250)
-        mesh_input = np.array(np.meshgrid(xs, ys))
-        # flatten mesh to allow forward pass
-        mesh_input = mesh_input.reshape(2, 250*250)
-        predictions = layer.forward(mesh_input)
-        # undo flatten to for squared image
-        predictions = predictions.reshape(250,250)
-        self.ax.contourf(xs, ys, predictions, levels=100, cmap=plt.cm.RdYlGn)
+        contourplot = ax.contourf(xs, ys, predictions, levels=100, cmap=plt.cm.RdYlGn)
         
-    def visualize_data(self, data_x, data_y):
-        scatterplot = self.ax.scatter(data_x[:, 0], data_x[:, 1], c=data_y)
+        # add legend to data
+        plt.colorbar(contourplot, label= "y_hat") 
+
+        
+    
+    @staticmethod
+    def visualize_data(ax, data_x, data_y):
+        scatterplot = ax.scatter(data_x[:, 0], data_x[:, 1], c=data_y)
+
+        # add legend
         labels = list(np.unique(data_y))
-        self.ax.legend(handles=scatterplot.legend_elements()[0], labels=labels)
+        ax.legend(handles=scatterplot.legend_elements()[0], labels=labels, title="y")
+
     @staticmethod
     def calculate_boundaries(features):
         x = features[:, 0]
@@ -75,5 +68,6 @@ class Visualization():
         boundaries = (x_min, x_max, y_min, y_max)
         return boundaries
         
-    def display_loss(self):
+    @staticmethod
+    def display_loss():
         pass

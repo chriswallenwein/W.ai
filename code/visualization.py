568ef7c0e2d1ab6daf5165f69a12500722d95bf5
cwallenwein <chris.wallenwein@gmail.com>
2022-11-29 16:49:34 +0100

    clarify naming (y axis !=  label)

--- code/visualization.py
+++ code/visualization.py
@@ -14,22 +14,22 @@ import matplotlib.pyplot as plt
 class Visualize():
 
     @staticmethod
-    def model(model, features, labels):
+    def model(model, x, y):
         if model.layers[0].in_dim != 2:
             raise ValueError("Input dimensions of layer must equal 2")
         
-        boundaries = Visualize.calculate_boundaries(features)
+        boundaries = Visualize.calculate_boundaries(x)
         ax = plt.axes()
         Visualize.visualize_model_weights(ax, model, boundaries)
-        Visualize.visualize_data(ax, features, labels)
+        Visualize.visualize_data(ax, x, y)
         plt.xlabel("x_1")
         plt.ylabel("x_2")
 
     @staticmethod
     def visualize_model_weights(ax, model, boundaries):
-        x_min, x_max, y_min, y_max = boundaries
-        xs = np.linspace(x_min, x_max, 250)
-        ys = np.linspace(y_min, y_max, 250)
+        x1_min, x1_max, x2_min, x2_max = boundaries
+        xs = np.linspace(x1_min, x1_max, 250)
+        ys = np.linspace(x2_min, x2_max, 250)
         mesh_input = np.array(np.meshgrid(xs, ys))
         # flatten mesh to allow forward pass
         mesh_input = mesh_input.reshape(2, 250*250)
@@ -44,28 +44,28 @@ class Visualize():
         
     
     @staticmethod
-    def visualize_data(ax, data_x, data_y):
-        scatterplot = ax.scatter(data_x[:, 0], data_x[:, 1], c=data_y)
+    def visualize_data(ax, x, y):
+        scatterplot = ax.scatter(x[:, 0], x[:, 1], c=y)
 
         # add legend
-        labels = list(np.unique(data_y))
+        labels = list(np.unique(y))
         ax.legend(handles=scatterplot.legend_elements()[0], labels=labels, title="y")
 
     @staticmethod
     def calculate_boundaries(features):
-        x = features[:, 0]
-        y = features[:, 1]
+        x1 = features[:, 0]
+        x2 = features[:, 1]
         
-        x_std = np.std(x)
-        y_std = np.std(y)
+        x1_std = np.std(x1)
+        x2_std = np.std(x2)
         
-        x_min = np.min(x) - x_std
-        y_min = np.min(y) - y_std
+        x1_min = np.min(x1) - x1_std
+        x2_min = np.min(x2) - x2_std
         
-        x_max = np.max(x) + x_std
-        y_max = np.max(y) + y_std
+        x1_max = np.max(x1) + x1_std
+        x2_max = np.max(x2) + x2_std
 
-        boundaries = (x_min, x_max, y_min, y_max)
+        boundaries = (x1_min, x1_max, x2_min, x2_max)
         return boundaries
         
     @staticmethod

b4c0baf32e833bd87f333b89069b7cbdfea4ae2b
cwallenwein <chris.wallenwein@gmail.com>
2023-01-11 16:04:21 +0100

    documentation for activation.py

--- source/activation.py
+++ source/activation.py
@@ -1,13 +1,22 @@
 import numpy as np
 
 class Activation():
+    """Base class for neural network activation functions"""
     def __init__(self):
         return
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray):
+        """Computes the output of the activation function.
+
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
 
-    def backward(self, x):
+    def backward(self, x: np.ndarray):
+        """Computes the gradient of the activation function.
+
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
     
 # class Softmax(Activation):
@@ -15,26 +24,66 @@ class Activation():
 #     def __init__(self):
 #         return
 
-#     def forward(self, x):
+#     def forward(self, x: np.ndarray):
 #         x = np.exp(x)
 #         total = np.sum(x, axis=0)
 #         return x/total
 
-#     def backward(self, x):
+#     def backward(self, x: np.ndarray):
 #         return
 
 class ReLU(Activation):
-    def forward(self, x):
+    """Rectified linear unit activation function"""
+
+    def forward(self, x: np.ndarray):
+        """Computes the output of the ReLU activation function.
+
+        Arguments:
+            x: np.array
+                input sample
+
+        Returns
+            np.array
+                output of ReLU
+        """
         self.cache = x
         return np.maximum(0, x)
 
     def backward(self):
+        """Computes the gradient of the ReLU activation function.
+        
+        Computes the local gradient of the ReLU activation function using the cached input during the forward pass.
+        """
         return np.heaviside(self.cache, 1)
 
 class Sigmoid(Activation):
-    def forward(self, x):
+    """Sigmoid activation function"""
+
+    def forward(self, x: np.ndarray):
+        """Computes the output of the Sigmoid activation function.
+
+        Arguments:
+            x: np.array
+                input sample
+
+        Returns
+            np.array
+                output of Sigmoid
+        """
         return 1/(1+np.exp(-x))
 
 class TanH(Activation):
-    def forward(self, x):
+    """TanH activation function"""
+
+    def forward(self, x: np.ndarray):
+        """Computes the output of the TanH activation function.
+
+        Arguments:
+            x: np.array
+                input sample
+
+        Returns
+            np.array
+                output of TanH
+        """
         return (np.exp(x)-np.exp(-x))/(np.exp(-x)+np.exp(x))

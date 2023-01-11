a0983b38c970240c89e5b1d1412a83fe7385a0b4
cwallenwein <chris.wallenwein@gmail.com>
2023-01-11 15:11:24 +0100

    full documentation of cost

--- source/cost.py
+++ source/cost.py
@@ -1,42 +1,44 @@
 import numpy as np
 
+# TODO add np.array-dimensions to docstrings
 
 class Cost():
-    """Base class for all cost functions.
-    """
+    """Base class for all cost functions."""
 
     def __init__(self):
         return
 
-    def forward(self, y, y_hat):
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
         """Forward pass of the cost function.
         
         Subclasses should implement this method
         """
         raise NotImplementedError
 
-    def backward(self, y, y_hat):
+    def backward(self, y: np.ndarray, y_hat: np.ndarray):
         """
-        Backward propagation of the cost function.
+        Computes the gradient of the cost function.
 
         Subclasses should implement this method
         """
         raise NotImplementedError
 
 class L1(Cost):
-    """
-    L1 cost function
-    """
-    def forward(self, y, y_hat):
+    """L1 cost"""
+
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
         """Forward pass of L1 cost.
 
         Computes the average elementwise difference between y and y_hat.
 
-        Args:
-            y:
+        Arguments:
+            y: np.array()
                 The ground truth label
-            y_hat:
+            y_hat: np.array()
                 The predicted label
+        
+        Returns:
+            scalar cost
         """
         self.y_cache = y
         self.y_hat_cache = y_hat
@@ -45,13 +47,34 @@ class L1(Cost):
         return avg_cost
 
     def backward(self):
+        """Computes gradient of the L1 cost.
+
+        Computes the local gradient of the L1 cost function using the cached y and y_hat.
+
+        Returns:
+            gradient of L1 cost
+        """
         cost_gradient = 1 / self.y_cache.size
         loss_gradient = np.sign(self.y_hat_cache - self.y_cache)
         return cost_gradient * loss_gradient
 
 class L2(Cost):
+    """L2 cost"""
 
-    def forward(self, y, y_hat):
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
+        """Forward pass of L2 cost.
+
+        Computes the average squared elementwise difference between y and y_hat.
+
+        Arguments:
+            y: np.array()
+                The ground truth label
+            y_hat: np.array()
+                The predicted label
+
+        Returns:
+            scalar cost
+        """
         self.y_cache = y
         self.y_hat_cache = y_hat
         elementwise_loss = np.square(y_hat - y)
@@ -59,6 +82,12 @@ class L2(Cost):
         return avg_cost
 
     def backward(self):
+        """Computes gradient of the L2 cost.
+        Computes the local gradient of the L1 cost function using the cached y and y_hat.
+
+        Returns:
+            gradient of L2 cost
+        """
         cost_gradient = 1 / self.y_cache.size
         loss_gradient = 2 * (self.y_hat_cache - self.y_cache)
         return cost_gradient * loss_gradient

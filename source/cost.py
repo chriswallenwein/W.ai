ecf5403c709bba294603409b66e585a2a14c464e
Christian Wallenwein <40916592+cwallenwein@users.noreply.github.com>
2023-01-11 17:09:13 +0100

    Add documentation
    Add docstrings to classes and methods

--- source/cost.py
+++ source/cost.py
@@ -1,20 +1,44 @@
 import numpy as np
 
-# average cost over entire tensor
+# TODO add np.array-dimensions to docstrings
+
 class Cost():
-    # func: e.g. np.sum, np.average
+    """Base class for all cost functions."""
+
     def __init__(self):
         return
 
-    def forward(self, y, y_hat):
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
+        """Computes the cost.
+        
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
 
-    def backward(self, y, y_hat):
+    def backward(self, y: np.ndarray, y_hat: np.ndarray):
+        """Computes the gradient of the cost function.
+
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
 
 class L1(Cost):
-    
-    def forward(self, y, y_hat):
+    """L1 cost"""
+
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
+        """Computes the L1 cost.
+
+        Computes the average elementwise difference between y and y_hat.
+
+        Arguments:
+            y: np.array()
+                The ground truth label
+            y_hat: np.array()
+                The predicted label
+        
+        Returns:
+            scalar cost
+        """
         self.y_cache = y
         self.y_hat_cache = y_hat
         elementwise_loss = np.absolute(y_hat - y)
@@ -22,13 +46,34 @@ class L1(Cost):
         return avg_cost
 
     def backward(self):
+        """Computes gradient of the L1 cost.
+
+        Computes the local gradient of the L1 cost function using the cached y and y_hat.
+
+        Returns:
+            gradient of L1 cost
+        """
         cost_gradient = 1 / self.y_cache.size
         loss_gradient = np.sign(self.y_hat_cache - self.y_cache)
         return cost_gradient * loss_gradient
 
 class L2(Cost):
+    """L2 cost"""
 
-    def forward(self, y, y_hat):
+    def forward(self, y: np.ndarray, y_hat: np.ndarray):
+        """Computes the L2 cost.
+
+        Computes the average squared elementwise difference between y and y_hat.
+
+        Arguments:
+            y: np.array()
+                The ground truth label
+            y_hat: np.array()
+                The predicted label
+
+        Returns:
+            scalar cost
+        """
         self.y_cache = y
         self.y_hat_cache = y_hat
         elementwise_loss = np.square(y_hat - y)
@@ -36,6 +81,12 @@ class L2(Cost):
         return avg_cost
 
     def backward(self):
+        """Computes gradient of the L2 cost.
+        Computes the local gradient of the L1 cost function using the cached y and y_hat.
+
+        Returns:
+            gradient of L2 cost
+        """
         cost_gradient = 1 / self.y_cache.size
         loss_gradient = 2 * (self.y_hat_cache - self.y_cache)
         return cost_gradient * loss_gradient
--- source/layer.py
+++ source/layer.py
@@ -1,48 +1,126 @@
 import numpy as np
 
-# TODO: implement Xavier initialization
+# TODO add np.array-dimensions to docstrings
 
 class Layer():
+    """Base class for all neural network layers."""
+
     def __init__(self):
         return
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray):
+        """Computes the forward pass of the layer.
+        
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
 
-    def backward(self, x):
+    def backward(self, x: np.ndarray):
+        """Computes the gradient of the layer.
+
+        Subclasses should implement this method.
+        """
         raise NotImplementedError
+
+class StandardFullyConnected(Layer):
+    """Fully connected layer"""
+
+    def __init__(self, in_dim: int, out_dim: int):
+        """Initializes the layer
+
+        Initializes the weights of the fully connected layer.
+
+        Arguments:
+            in_dim: int > 0
+                dimension of one input sample
+            out_dim: int > 0
+                dimension of one output sample
+        """
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.w = 2*np.random.rand(out_dim, in_dim) -1
+        self.b = 2*np.random.rand(out_dim, 1) -1
+
+    def forward(self, x: np.ndarray):
+        """Computes the output of the layer.
+        
+        Computes forward pass of the fully connected layer for n samples
+
+        Arguments:
+            x: np.array
+                n input samples
+
+        Returns:
+            np.array
+                output of the layer
+        """
+        y_hat = self.w@x + self.b
+        self.cache = x
+        return y_hat
+
+    def backward(self, x: np.ndarray):
+        """Computes the gradient of the layer.
+        
+        Arguments:
+            x: np.array
+                n input samples
+
+        Returns:
+            gradient of the layer
+        """
+        return x
 
 class FullyConnectedBiasTrick(Layer):
+    """Fully connected layer with bias trick.
 
-    def __init__(self, in_dim, out_dim):
+    Here's a simple explanation of the bias trick:
+    https://hetpinvn.wordpress.com/2016/10/26/bias-trick/
+    """
+
+    def __init__(self, in_dim: int, out_dim: int):
+        """Initializes the layer.
+
+        Initializes the weights of the fully connected layer.
+
+        Arguments:
+            in_dim: int > 0
+                dimension of one input sample
+            out_dim: int > 0
+                dimension of one output sample
+        """
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.w = 2*np.random.rand(out_dim, in_dim+1) -1
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray):
+        """Computes the output of the layer.
+        
+        Computes forward pass of the fully connected layer with the bias trick for n samples
+
+        Arguments:
+            x: np.array
+                n input samples
+
+        Returns:
+            np.array
+                output of the layer
+        """
         ones = np.ones((1, x.shape[1]))
         x = np.concatenate((ones, x))
         self.cache = x
         y_hat = self.w@x # the bias trick
         return y_hat
 
-    def backward(self, x):
+    def backward(self, x: np.ndarray):
+        """Computes the gradient of the layer.
+        
+        Arguments:
+            x: np.array
+                n input samples
+
+        Returns:
+            gradient of the layer
+        """
         return x
 
-class FullyConnectedNormal(Layer):
-
-    def __init__(self, in_dim, out_dim):
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.w = 2*np.random.rand(out_dim, in_dim) -1
-        self.b = 2*np.random.rand(out_dim, 1) -1
-
-    def forward(self, x):
-        y_hat = self.w@x + self.b
-        self.cache = x
-        return y_hat
-
-    def backward(self, x):
-        return x
-
-FullyConnected = FullyConnectedNormal
+FullyConnected = StandardFullyConnected

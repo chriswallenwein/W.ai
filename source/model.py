ee12b06c6401b020347bec6806e04d4af382d675
cwallenwein <chris.wallenwein@gmail.com>
2023-01-11 15:52:21 +0100

    add documentation to model.py

--- source/model.py
+++ source/model.py
@@ -1,11 +1,31 @@
-import numpy as np
+# TODO: check if the dimensions of the different layers fit together
 
 class Model():
+    """Neural network model, contains multiple layers"""
+
     def __init__(self, layers):
+        """Initializes the model with a list of layers
+
+        Arguments:
+            layers: List[Layer]
+                List of all layers of the model
+        """
         self.layers = layers
-        return
 
     def forward(self, x):
+        """Computes output of the entire model
+
+        Iterates through the layers of the model. It computes the output of each layer
+        and uses it as the input for the next layer.
+        
+        Arguments:
+            x: np.array
+                Model input
+
+        Returns:
+            np.array
+                Output of the last layer
+        """
         result = x
         for layer in self.layers:
             result = layer.forward(result)
